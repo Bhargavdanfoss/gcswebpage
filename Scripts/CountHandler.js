ba8dd6491e65a7c34c7027c6fc753b34a3ff1dea
Bhargavdanfoss <bhargav.danfoss@gmail.com>
2024-08-06 10:15:18 +0530

    comment

---
+++ Scripts/CountHandler.js
@@ -0,0 +1,24 @@
+function sendCount(PageCount,PageName)
+{/*
+    console.log("sendCount Entry");   
+
+    var jsonData = JSON.stringify({
+        "pagecount":PageCount,        
+        "pagename":PageName
+    });
+    
+    $.ajax({
+        type: "POST", 
+        contentType: "application/json; charset=utf-8",
+        url: "https://prod-87.westeurope.logic.azure.com/workflows/d0b8d7b3702c41e0a2f24e4f9cd18fb4/triggers/manual/paths/invoke/Send?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2Q0klkNsUInggoO1Il8UFfBVJJ1RcESRKf91fcHaRyo",
+        dataType: "json",
+        data: jsonData,
+        success: function (data) {
+           console.log('success',data); 
+           
+        }
+    });   
+    
+    alert(JSON.stringify(jsonData));
+    console.log("sendCount Exit");
+*/}
